b26caf865f927b4c9dec496afe2f83cc14a631d2
Tony Matthes <tony@Tonys-MacBook-Pro.local>
2019-01-29 16:48:27 -0600

    add conditional rendering of subtotal bar and order nav item

--- src/components/SubtotalBar/SubtotalBar.js
+++ src/components/SubtotalBar/SubtotalBar.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class SubTotalBar extends Component {
 
-  state = {
-    order: [],
-  }
 
   render() {
     return (
       <div>
         <h4>
           {this.props.state.order.orderItems.orders.length} items,
-          Subtotal: ${this.props.state.order.orderItems.orders.length < 1? '0.00':
-          this.props.state.order.orderItems.orders.map(item => (Number(item.price))).reduce((accumulator, currentValue) => accumulator + currentValue).toFixed(2)
+          Subtotal: ${this.props.state.order.orderItems.orders.length < 1 ? '0.00' :
+            this.props.state.order.orderItems.orders.map(item =>
+              (Number(item.price))).reduce((accumulator, currentValue) => accumulator + currentValue).toFixed(2)
           }</h4>
+        <Link className="nav-link" to="/currentorder">
+          view current order
+          </Link>
       </div>
     );
   }
