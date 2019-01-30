fb3c73735021b14ccaeda5ba3a8bb4057376a04b
Tony Matthes <tony@Tonys-MacBook-Pro.local>
2019-01-29 18:12:31 -0600

    clean up for github

--- src/components/SubtotalBar/SubtotalBar.js
+++ src/components/SubtotalBar/SubtotalBar.js
@@ -7,17 +7,12 @@ class SubTotalBar extends Component {
 
   render() {
     return (
-      <div>
-        <h4>
-          {this.props.state.order.orderItems.orders.length} items,
-          Subtotal: ${this.props.state.order.orderItems.orders.length < 1 ? '0.00' :
-            this.props.state.order.orderItems.orders.map(item =>
-              (Number(item.price))).reduce((accumulator, currentValue) => accumulator + currentValue).toFixed(2)
-          }</h4>
-        <Link className="nav-link" to="/currentorder">
-          view current order
-          </Link>
-      </div>
+      <Link className="nav-link" to="/currentorder">
+        Cart: {this.props.state.order.orderItems.orders.length} {this.props.state.order.orderItems.orders.length === 1 ? 'item' : 'items'},
+          Subtotal: ${this.props.state.order.orderItems.orders.map(item => (Number(item.price))
+        ).reduce((accumulator, currentValue) => accumulator + currentValue
+        ).toFixed(2)}
+      </Link>
     );
   }
 }
